ab8d97167a8d46530bce9699a6f99ef4922a01cd
Ariel Lorenzo <ariellorenzo08@gmail.com>
2020-03-27 13:18:40 -0400

    Initiation

--- Metronome/MainWindow.xaml.cs
+++ Metronome/MainWindow.xaml.cs
@@ -13,16 +13,54 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Metronome
-{
+namespace Metronome {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
+
+        #region Event Handlers
+        /// <summary>
+        /// Bring tempo up
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Up_Click(object sender, RoutedEventArgs e) {
+            int currentTempo = int.Parse(Tempo.Content.ToString());
+            Tempo.Content = (currentTempo < 218) ?
+                (currentTempo + 1).ToString() : 218.ToString();
+        }
+
+        /// <summary>
+        /// Bring tempo down
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Down_Click(object sender, RoutedEventArgs e) {
+            int currentTempo = int.Parse(Tempo.Content.ToString());
+            Tempo.Content = (currentTempo > 0) ?
+                (currentTempo - 1).ToString() : 0.ToString();
+        }
+        #endregion
+
+        #region Beat Generation
+        /// <summary>
+        /// Play a simple beat
+        /// </summary>
+        private void PlayBeat() {
+            // .....
+        }
+
+        /// <summary>
+        /// Play metronome according to tempo provided as a function parameter
+        /// </summary>
+        /// <param name="tempo"></param>
+        private void GenerateTempo(int tempo) {
+            // .....
+        }
+        #endregion
     }
 }
